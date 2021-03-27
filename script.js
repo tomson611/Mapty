00ce985fb427e472a3c0c978329d2b0283934478
Tomson <twesolowski61@gmail.com>
2021-03-27 14:22:23 +0100

    displaying a map using leaflet

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ if (navigator.geolocation)
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.co.uk/maps/@${latitude}.${longitude}`);
+
+      const cords = [latitude, longitude];
+      const map = L.map('map').setView(cords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(cords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not tet your positon');
