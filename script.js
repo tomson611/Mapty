456c85a10b42effe29873ff776e657ce27a97459
Tomson <twesolowski61@gmail.com>
2021-03-27 15:34:23 +0100

    displaying a map marker

--- script.js
+++ script.js
@@ -18,18 +18,30 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(`https://www.google.co.uk/maps/@${latitude}.${longitude}`);
 
-      const cords = [latitude, longitude];
-      const map = L.map('map').setView(cords, 13);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(cords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        L.marker(mapEvent.latlng)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not tet your positon');
